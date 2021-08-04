bae699325747ea6b63145849e6b76e391f999c5a
Denis Pirogov <icestrelok@gmail.com>
2021-08-04 21:12:56 +0300

    created a script 'mils' emulating the utility 'ls -l'

--- file_handling/myls.sh
+++ file_handling/myls.sh
@@ -7,5 +7,7 @@ fi
 
 for f in $path
 do
-        stat --printf "%A %F %U %G %s %z %n\n" ${f}
+	sec=$(stat --printf "%Z" ${f})
+	time=$(date -d @$sec '+%Y-%m-%d %H:%H')
+	stat --printf "%A %h %U %G %s ${time} %n\n" ${f}
 done
